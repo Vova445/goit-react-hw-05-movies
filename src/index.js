4de6a750fd6f35efae5a786ce3529e497ecc22fd
Volodya Mikhnov <vvvmikhnov@gmail.com>
2023-12-22 20:11:11 +0200

    result

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  
     <BrowserRouter basename="/goit-react-hw-05-movies">
+      <React.StrictMode>
       <App />
+      </React.StrictMode>
     </BrowserRouter>
-  </React.StrictMode>
+  
 );
